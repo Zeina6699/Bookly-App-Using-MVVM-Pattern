61c50ddcda0357715499005b5f07e888e353cbe4
Zeina <zenahhamad25@gmail.com>
2024-04-27 13:19:43 -0400

    Adding animation to splashView

--- lib/features/SplashView/Screens/widgets/Splash_View_Body.dart
+++ lib/features/SplashView/Screens/widgets/Splash_View_Body.dart
@@ -1,17 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 // ignore: camel_case_types
-class splashViewBody extends StatelessWidget {
+class splashViewBody extends StatefulWidget {
   const splashViewBody({super.key});
+
+  @override
+  State<splashViewBody> createState() => _splashViewBodyState();
+}
+
+class _splashViewBodyState extends State<splashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: 
+        Duration(seconds: 2));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 7), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+    slidingAnimation.addListener(() {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.stretch
-     , children: [
-        Image.asset('assets/images/Logo.png')
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset('assets/images/Logo.png'),
+        const SizedBox(height: 4),
+        SlideTransition(
+            position: slidingAnimation,
+            child: const Text('Read Free Books'
+            ,style: TextStyle(fontSize: 16)
+            , textAlign: TextAlign.center))
       ],
     );
   }
